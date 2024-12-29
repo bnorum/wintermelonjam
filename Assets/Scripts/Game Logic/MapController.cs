ad2105087ffd3c7becc54ce7ef2f1268b2be9590
dylanthy <mdfdylanyt@gmail.com>
2024-12-28 21:27:39 -0500

    Grid Optimization
    Removes tiles that are far from user

--- Assets/Scripts/Game Logic/MapController.cs
+++ Assets/Scripts/Game Logic/MapController.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 public class MapController : MonoBehaviour
 {
     public List<GameObject> terrainChunks;
@@ -10,6 +12,14 @@ public class MapController : MonoBehaviour
 
     public GameObject currentChunk;
     PlayerMovement pm;
+
+    [Header("Optimization")]
+    public List<GameObject> spawnedChunks;
+    GameObject latestChunk;
+    public float maxOpDistance;
+    float opDistance;
+    float opCooldown;
+    float opCooldownDuration;
     void Start()
     {
         pm = FindFirstObjectByType<PlayerMovement>();
@@ -19,6 +29,7 @@ public class MapController : MonoBehaviour
     void Update()
     {
         ChunkChecker();
+        ChunkOptimizer();
     }
 
     void ChunkChecker()
@@ -33,6 +44,10 @@ public class MapController : MonoBehaviour
             {
                 noTerrainPosition = currentChunk.transform.Find("Right Chunk").position;
                 SpawnChunk();
+                noTerrainPosition =currentChunk.transform.Find("BottomRight Chunk").position;
+                SpawnChunk();
+                noTerrainPosition =currentChunk.transform.Find("TopLeft Chunk").position;
+                SpawnChunk();
             }
         }
         else if(pm.movement.x < 0 && pm.movement.y == 0) // left
@@ -40,6 +55,10 @@ public class MapController : MonoBehaviour
             if(!Physics2D.OverlapCircle(currentChunk.transform.Find("Left Chunk").position, checkerRadius, terrainMask))
             {
                 noTerrainPosition =currentChunk.transform.Find("Left Chunk").position;
+                SpawnChunk();
+                noTerrainPosition =currentChunk.transform.Find("BottomLeft Chunk").position;
+                SpawnChunk();
+                noTerrainPosition =currentChunk.transform.Find("TopLeft Chunk").position;
                 SpawnChunk();
             }
         }
@@ -49,6 +68,10 @@ public class MapController : MonoBehaviour
             {
                 noTerrainPosition =currentChunk.transform.Find("Top Chunk").position;
                 SpawnChunk();
+                noTerrainPosition =currentChunk.transform.Find("TopRight Chunk").position;
+                SpawnChunk();
+                noTerrainPosition =currentChunk.transform.Find("TopLeft Chunk").position;
+                SpawnChunk();
             }
         }
         else if(pm.movement.x == 0 && pm.movement.y < 0) // down
@@ -57,36 +80,8 @@ public class MapController : MonoBehaviour
             {
                 noTerrainPosition =currentChunk.transform.Find("Bottom Chunk").position;
                 SpawnChunk();
-            }
-        }
-        else if(pm.movement.x > 0 && pm.movement.y > 0) // up right
-        {
-            if(!Physics2D.OverlapCircle(currentChunk.transform.Find("TopRight Chunk").position, checkerRadius, terrainMask))
-            {
-                noTerrainPosition =currentChunk.transform.Find("TopRight Chunk").position;
-                SpawnChunk();
-            }
-        }
-        else if(pm.movement.x < 0 && pm.movement.y > 0) // up left
-        {
-            if(!Physics2D.OverlapCircle(currentChunk.transform.Find("TopLeft Chunk").position, checkerRadius, terrainMask))
-            {
-                noTerrainPosition =currentChunk.transform.Find("TopLeft Chunk").position;
-                SpawnChunk();
-            }
-        }
-        else if(pm.movement.x > 0 && pm.movement.y < 0) // down right
-        {
-            if(!Physics2D.OverlapCircle(currentChunk.transform.Find("BottomRight Chunk").position, checkerRadius, terrainMask))
-            {
                 noTerrainPosition =currentChunk.transform.Find("BottomRight Chunk").position;
                 SpawnChunk();
-            }
-        }
-        else if(pm.movement.x < 0 && pm.movement.y < 0) // down left
-        {
-            if(!Physics2D.OverlapCircle(currentChunk.transform.Find("BottomLeft Chunk").position, checkerRadius, terrainMask))
-            {
                 noTerrainPosition =currentChunk.transform.Find("BottomLeft Chunk").position;
                 SpawnChunk();
             }
@@ -96,6 +91,32 @@ public class MapController : MonoBehaviour
     void SpawnChunk()
     {
         int rand = Random.Range(0,terrainChunks.Count);
-        Instantiate(terrainChunks[rand], noTerrainPosition, Quaternion.identity);
+        latestChunk = Instantiate(terrainChunks[rand], noTerrainPosition, Quaternion.identity);
+        spawnedChunks.Add(latestChunk);   
+    }
+
+    void ChunkOptimizer()
+    {
+        opCooldown -= Time.deltaTime;
+
+        if(opCooldown <= 0f)
+        {
+            opCooldown = opCooldownDuration;
+
+        }
+        else
+            return;
+        foreach(GameObject chunk in spawnedChunks)
+        {
+            opDistance = Vector3.Distance(player.transform.position, chunk.transform.position);
+            if(opDistance > maxOpDistance)
+            {
+                chunk.SetActive(false);
+            }
+            else
+            {
+                chunk.SetActive(true);
+            }
+        }
     }
 }
